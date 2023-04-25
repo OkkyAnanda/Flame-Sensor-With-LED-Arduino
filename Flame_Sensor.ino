ec2e69031daaabbbae0b0a8b6ede563c6bd78589
OkkyAnanda <131729834+OkkyAnanda@users.noreply.github.com>
2023-04-25 08:56:11 +0700

    Add files via upload

---
+++ Flame_Sensor.ino
@@ -0,0 +1,45 @@
+int SensorPin=A0;
+int SensorValue=0;
+#include <Servo.h>
+Servo servoLeft;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+servoLeft.attach(10);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+ SensorValue=analogRead(SensorPin);
+Serial.println(SensorValue);
+delay(100);
+if (SensorValue<=200){
+Serial.println("LAMPU 1 MATI");
+turnRight();
+}else{
+ Serial.println("LAMPU 1 NYALA");
+turnLeft();
+}
+}
+void forward()  {
+  servoLeft.write(0);
+  
+}
+
+void reverse()  {
+  servoLeft.write(180);
+  
+}
+
+void turnRight()  {
+  servoLeft.write(180);
+  
+}
+void turnLeft() {
+  servoLeft.write(0);
+  
+}
+void stopRobot()  {
+  servoLeft.write(90);
+  
+}
+
